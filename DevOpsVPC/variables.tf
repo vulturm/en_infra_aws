b02f574fa760eb3a3e4bdc29f0f2ce25d694ee54
Mihai Vultur <mihai.vultur@endava.com>
2017-04-03 14:55:54 +0000

    Variables: renamed some of the variables to better reflect their purpose

--- DevOpsVPC/variables.tf
+++ DevOpsVPC/variables.tf
@@ -8,23 +8,32 @@
 # All rights reserved
 #
 # Description:
-#   Variables we're using.
+#   Variables we're using with their description.
 #   If a default value is set, the variable is optional.
 #   Otherwise, the variable is required.
 
-variable "name" {}
-variable "vpc_cidr" {}
-
-variable "name" {
-  default = "terraform-test-vpc"
-}
-
-variable "region" {
+#-- Naming
+variable "aws_region" {
   description = "AWS region"
-  default = "us-east-1"
+  default = "us-east-1" 
+}
+variable "aws_azs" {
+  description = "AWS Availability zones"
+}
+variable "vpc_name" {
+  description = "Name of the VPC"
 }
 
-variable "cidr" {}
+#-- Networking
+variable "vpc_cidr"        {
+  description = "The CIDR block for our VPC"
+}
+variable "vpc_private_subnets" {
+  description = "The CIDR blocks for our private subnets"
+ }
+variable "vpc_public_subnets"  {
+  description = "The CIDR blocks for our public subnets"
+}
 
 variable "enable_dns_support" {
   description = "should be true if you want to use private DNS within the VPC"
@@ -36,6 +45,7 @@ variable "enable_dns_hostnames" {
   default = false
 }
 
+#-- Tags for accounting
 variable "default_tags" {
   description = "A map of tags to add to all resources"
   type        = "map"
