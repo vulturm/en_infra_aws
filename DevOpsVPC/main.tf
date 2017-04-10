f085cbbac7ffa023636df3fdb1f0d265b3a27a0f
Mihai Vultur <mihai.vultur@endava.com>
2017-04-10 10:17:23 +0000

    VPC: Implement network security groups used for public and private subnets

--- DevOpsVPC/main.tf
+++ DevOpsVPC/main.tf
@@ -100,3 +100,73 @@ resource "aws_route_table_association" "private" {
   lifecycle { create_before_destroy = true }
 }
 
+
+# Security Group #
+resource "aws_security_group" "AllowICMP" {
+  vpc_id      = "${aws_vpc.vpc.id}"
+  description = "Allow all ICMP traffic"
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = "${merge(var.default_tags, map("VPC", var.vpc_name), map("Name", format("%s_SG_%s", var.vpc_name, "AllowICMP")))}"
+}
+
+resource "aws_security_group" "DefaultPub" {
+  vpc_id      = "${aws_vpc.vpc.id}"
+  description = "Default VPC security group for public facing IPs"
+
+  # TCP access
+  ingress {
+    from_port = 0
+    to_port = 65535
+    protocol = "tcp"
+    cidr_blocks = ["194.126.146.0/24"]
+  }
+
+  # HTTP access from anywhere
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound acces to anywhere
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = "${merge(var.default_tags, map("VPC", var.vpc_name), map("Name", format("%s_SG_%s", var.vpc_name, "DefaultPub")))}"
+}
+
+resource "aws_security_group" "DefaultPrv" {
+  vpc_id      = "${aws_vpc.vpc.id}"
+  description = "Default VPC security group for private IPs"
+
+  # Permit access from the VMs that resides in Public Subnet
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    security_groups = ["${aws_security_group.DefaultPub.id}"]
+  }
+
+  # Outbound acces to anywhere
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = "${merge(var.default_tags, map("VPC", var.vpc_name), map("Name", format("%s_SG_%s", var.vpc_name, "DefaultPrv")))}"
+}
+
