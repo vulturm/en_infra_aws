8c36c6ea722030de1fd4a9fea2d044c2ff20bca8
Mihai Vultur <mihai.vultur@endava.com>
2017-04-14 11:43:30 +0000

    Version 1.2.0 `main.tf` config was split in multiple `.tf` files according to their purpose.

---
+++ DevOpsVPC/outputs.tf
@@ -0,0 +1,34 @@
+#
+# Project Name:: en_infra_aws
+# File:: outputs.tf
+#
+# Copyright (C) 2017 - Present
+# Author: 'Mihai Vultur <mihai.vultur@endava.com>'
+#
+# All rights reserved
+#
+# Description:
+#   Outputs the following variables to the user.
+#
+
+#-- EC2 Instances
+output "NAT_instance_id" {
+    value = "${aws_instance.NatInstance.id}"
+}
+
+output "NAT_private_ip" {
+    value = "${aws_instance.NatInstance.private_ip}"
+}
+
+output "NAT_public_ip" {
+    value = "${aws_instance.NatInstance.public_ip}"
+}
+
+#--
+output "TEST_instance_id" {
+    value = "${aws_instance.TestInstance.id}"
+}
+
+output "TEST_private_ip" {
+    value = "${aws_instance.TestInstance.private_ip}"
+}
