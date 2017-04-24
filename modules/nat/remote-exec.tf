a362a25a490c8ec205c49762d90183e3ef2c83b4
Mihai Vultur <mihai.vultur@endava.com>
2017-04-24 09:41:00 +0000

    Version 1.4.0 Separate NAT instance configuration using a 'terraform module'.

---
+++ modules/nat/remote-exec.tf
@@ -0,0 +1,38 @@
+#
+# Project Name:: en_infra_aws
+# Module:: NAT
+#
+# Copyright (C) 2017 - Present
+# Author: 'Mihai Vultur <mihai.vultur@endava.com>'
+#
+# All rights reserved
+#
+# Description:
+#   Execute the required configuration for the EC2 instance to
+#     convert it into a fully functional NAT instance
+#
+
+#-- workarround adding a triggered resource
+#-- because of a circular dependency between
+#-- eip and aws_instance
+resource "null_resource" "preparation" {
+  triggers {
+    association_ip_address = "${aws_eip_association.eip_assoc_NatInstance.id}"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo iptables -t nat -C POSTROUTING -o eth0 -s ${var.private_subnets_cidr} -j MASQUERADE 2> /dev/null || sudo iptables -t nat -A POSTROUTING -o eth0 -s ${var.private_subnets_cidr} -j MASQUERADE",
+      "echo 'net.ipv4.ip_forward=1' | sudo tee -a /etc/sysctl.conf",
+      "echo 'net.ipv4.conf.eth0.send_redirects=0' | sudo tee -a /etc/sysctl.conf",
+      "echo 'net.netfilter.nf_conntrack_max=131072' | sudo tee -a /etc/sysctl.conf",
+      "sudo sysctl -p"
+    ]
+    connection {
+      host        = "${aws_eip.NatInstance.public_ip}"
+      user        = "centos"
+      timeout     = "30s"
+      private_key = "${file(var.private_key_file)}"
+    }
+  }
+}
+
---
+++ modules/nat/variables.tf
@@ -0,0 +1,62 @@
+#
+# Project Name:: en_infra_aws
+# Module:: NAT
+#
+# Copyright (C) 2017 - Present
+# Author: 'Mihai Vultur <mihai.vultur@endava.com>'
+#
+# All rights reserved
+#
+# Description:
+#   Module specific variables
+#
+
+variable "instance_name" {
+  description = "Name of the Nat instance that will appear in AWS Console"
+}
+variable "instance_type" {
+  description = "Type of the instance used that will serve as NAT Purpose"
+  default = "t2.micro"
+}
+variable "vpc_name" {
+  description = "VPC name that the created instance will be assigned to"
+}
+variable "vpc_id" {
+  description = "VPC ID that the instance will be assigned to"
+}
+variable "subnet_id" {
+  description = "Subnet ID that will be used for instance interface creation. Eg. Public Subnet ID."
+}
+variable "private_subnets_cidr" {
+  description = "CIDR of the private subnet that the instance will do NAT translation for"
+}
+variable "ami_id" {
+  description = "AWS AMI ID used for instance creation"
+}
+variable "user_data" {
+  description = "user_data config used during instance creation"
+}
+variable "sgs" {
+  description = "Security groups IDs that will be assigned to the NAT instance"
+}
+variable "key_name" {
+  description = "AWS Name of the ssh key to be used during instance provisioning"
+}
+variable "private_key_file" {
+  description = "Path to private key file use to connect to the NAT Instance"
+}
+
+variable "number_of_instances" {
+  description = "Number of instances to start"
+  default = 1
+}
+
+variable "root_volume_size" {
+    description = "Size of the root volume"
+    default = 8
+}
+
+variable "inbound_ports" {
+  description = "Comma separated list of ports that will be opened on the public facing IP of the NAT instance"
+  default  = "22,443"
+}
